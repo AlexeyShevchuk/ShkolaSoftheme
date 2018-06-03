edcfe0d58ab9fcc086851b8ab43ec5d4350dd652
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-06-03 17:53:53 +0300

    Lesson2HW1Done

--- CSharpHW/2/HW2/HW2/MainWindow.xaml.cs
+++ CSharpHW/2/HW2/HW2/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace HW2
 {
@@ -23,6 +10,64 @@ namespace HW2
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Culc(string operation)
+        {
+            if (operand1.Text == string.Empty || operand2.Text == string.Empty)
+            {
+                return;
+            }
+
+            if (!double.TryParse(operand1.Text, out double a) || !double.TryParse(operand2.Text, out double b))
+            {
+                operand1.Text = "0";
+                operand2.Text = "0";
+                resultextbox.Text = string.Empty;
+                return;
+            }
+
+            switch (operation)
+            {
+                case "+": a += b; break;
+                case "-": a -= b; break;
+                case "*": a *= b; break;
+                case "/":
+                    if (b == 0)
+                    {
+                        MessageBox.Show("Division by zero!");
+                        resultextbox.Text = string.Empty;
+                        return;
+                    }
+                    a /= b;
+                    break;
+                default: resultextbox.Text = string.Empty; return;
+            }
+            resultextbox.Text = string.Format("{0:f}",a);
+        }
+
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+            Culc("+");
+        }
+
+        private void SubtractClick(object sender, RoutedEventArgs e)
+        {
+            Culc("-");
+        }
+
+        private void MultiplyClick(object sender, RoutedEventArgs e)
+        {
+            Culc("*");
+        }
+
+        private void DivideClick(object sender, RoutedEventArgs e)
+        {
+            Culc("/");
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
