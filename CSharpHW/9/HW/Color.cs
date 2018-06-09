66d2101ab59d43ce9f5b98447f4a02d588933857
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-06-09 19:11:31 +0300

    Lesson9HWDone

---
+++ CSharpHW/9/HW/Color.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HW
+{
+    enum Color : int
+    {
+        Color1,
+        Color2,
+        Color3,
+        Color4,
+        Color5,
+        NewColor
+    }
+}
---
+++ CSharpHW/9/HW/Transmission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW
+{
+    enum Transmission : int
+    {
+        Transmission1,
+        Transmission2,
+        Transmission3,
+        Transmission4,
+        Transmission5,
+        NewTransmission
+    }
+}
