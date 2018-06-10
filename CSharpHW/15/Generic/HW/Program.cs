ca3e153dc068454deac216e4e6de1e2dd9ce22b4
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-06-10 23:02:25 +0300

    Lesson15GenericHWDone

---
+++ CSharpHW/15/Generic/HW/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            list.Remove(4);
+
+            var array = list.ToArray();
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
