d5965150c165193e74755a0e1ee4c8acad1c2934
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-06-10 19:29:29 +0300

    Lesson11HWDone

---
+++ CSharpHW/11/HW/HW/PhotoPrinter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW
+{
+    public class PhotoPrinter : Printer
+    {
+        public override void Print(string str)
+        {
+            Console.WriteLine("PhotoPrinter override: " + str);
+        }
+
+        public void Print(Photo photo)
+        {
+            Console.WriteLine("PhotoPrinter photo: " + photo.ToString());
+        }
+
+    }
+}
---
+++ CSharpHW/11/HW/HW2/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using HW;
+
+namespace HW2
+{
+    static class Extensions
+    {
+        public static void Print(this Printer printer, string[] text)
+        {
+            foreach (var str in text)
+            {
+                printer.Print(str);
+            }
+        }
+
+        public static void Print(this ColourPrinter colourPrinter, string[] text, ConsoleColor[] colors)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i > colors.Length - 1)
+                {
+                    colourPrinter.Print(text[i], colors[0]);
+                }
+                else
+                {
+                    colourPrinter.Print(text[i], colors[i]);
+                }
+            }
+        }
+
+        public static void Print(this PhotoPrinter photoPrinter, Photo[] photos)
+        {
+            foreach (var photo in photos)
+            {
+                photoPrinter.Print(photo);
+            }
+        }
+    }
+}
