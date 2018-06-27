af3d4372747f99d03bbdca8de9be5cb99c6ae82b
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-06-27 06:40:00 +0300

    Simple WCF Service Done

---
+++ CSharpHW/30/WcfClient/WcfClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ServiceModel;
+using System.Diagnostics;
+
+namespace WcfClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            var channelFactory = new ChannelFactory<IKeyContract>(new BasicHttpBinding(), new EndpointAddress("http://localhost:8000/"));
+
+            var service = channelFactory.CreateChannel();
+
+            var clientId = Process.GetCurrentProcess().Id;
+
+            Console.WriteLine("Client: Press key for send");
+            Console.WriteLine("Client: For exit press End");
+            var keyOut = Console.ReadKey();
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Client: Key out: {0}", keyOut.Key);
+
+                try
+                {
+                    var keyResponse = service.GetKey(keyOut.Key, clientId);
+                    Console.WriteLine("Client: Key response: {0}", keyResponse);
+                }
+                catch (EndpointNotFoundException e)
+                {
+                    Console.WriteLine("Server is not available");
+                    Console.WriteLine("Try again");
+                }
+
+                keyOut = Console.ReadKey();
+            }
+            while (keyOut.Key != ConsoleKey.End);
+
+            service.GetKey(keyOut.Key, clientId);
+        }
+    }
+}
---
+++ CSharpHW/30/WcfHost/WcfHost/KeyService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WcfHost
+{
+    class KeyService : IKeyContract
+    {
+        public ConsoleKey GetKey(ConsoleKey key, int clientId)
+        {
+            Console.WriteLine("Host got key: {0} from Client: {1}",key, clientId);
+
+            if (key == ConsoleKey.End)
+            {
+                Console.WriteLine("Client: {0} completed the session.", clientId);
+            }
+
+            return key;
+        }
+    }
+}
