a6307dc0a37adb9f85ddbea45ac5cff8d8ea72b6
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-06-27 07:58:43 +0300

    C#Lesson 23 not finished

---
+++ CSharpHW/30/WcfHost/WcfHost/IKeyContract.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace WcfHost
+{
+    [ServiceContract]
+    public interface IKeyContract
+    {
+        [OperationContract]
+        ConsoleKey GetKey(ConsoleKey key, int clientId);
+    }
+}
---
+++ CSharpHW/30/WcfHost/WcfHost/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+
+            var serviceHost = new WebServiceHost(typeof(KeyService), new Uri("http://localhost:8000/"));
+
+            serviceHost.AddServiceEndpoint(typeof(IKeyContract), new BasicHttpBinding(), "");
+
+            serviceHost.Open();
+
+            Console.WriteLine("Press any key for exit.");
+            Console.ReadKey();
+
+            serviceHost.Close();
+        }
+    }
+}
