ea333d8d1064a4ba7d3ccc7af99e4c5e4c8abcc4
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-06-28 19:17:06 +0300

    Test component

--- Frontend/Angular/my-app/src/app/app.module.ts
+++ Frontend/Angular/my-app/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		TestComponent
+		],
+	imports: [BrowserModule],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Frontend/Angular/my-app/src/app/test/test.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+     
+@Component({
+    selector: 'my-app',
+    templateUrl: './test.component.html'
+})
+export class TestComponent { 
+    name: "";
+}
