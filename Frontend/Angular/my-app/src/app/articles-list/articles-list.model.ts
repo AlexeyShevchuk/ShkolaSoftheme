a4eea09d52af848b643ebf4a2151aac648a57f14
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-07-02 05:29:19 +0300

    AngularHW3Done

--- Frontend/Angular/my-app/src/app/articles-list/articles-list.model.ts
+++
@@ -1,8 +0,0 @@
-import { ArticleDetailsComponent } from './article-details/article-details.component';
-
-export { ArticleDetailsModel } from './article-details/article-details.model';
-
-export interface ArticlesListModel {
-    articles: ArticleDetailsComponent[];
-}
-
